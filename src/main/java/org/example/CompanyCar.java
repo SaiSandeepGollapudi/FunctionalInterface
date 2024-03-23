479d121dd7e6d6db3a35a966598ba5f2a79e5c25
SaiSandeepGollapudi <gsandeep2406@gmail.com>
2024-03-23 16:34:03 -0400

    first commit

---
+++ src/main/java/org/example/CompanyCar.java
@@ -0,0 +1,13 @@
+package org.example;
+//extends -> inherit the properties -> applicable for 'class -> class' or 'interface -> interface'
+public interface CompanyCar extends Car {
+
+    void openAirBag();//Interface methods are by default abstract and public
+
+    void closeAirBag();
+
+    default void addFrontLogo(int name){
+        System.out.println("I am coming from interface with default method");
+    }
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        ACar car1 = new ACar();
+        car1.addFrontLogo();
+        car1.addFrontLogo(1);
+        car1.addFrontLogo("abc");
+    }
+}
